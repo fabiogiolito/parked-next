9fa82883acd1243ca4f171e763680559311b4e79
Fabio Giolito <fabiogiolito@gmail.com>
2021-05-12 01:38:57 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,6 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-
-  const { protocol, host } = absoluteUrl(req)
-
   return (
     <div className={styles.container}>
       <Head>
