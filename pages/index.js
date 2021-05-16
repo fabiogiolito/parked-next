529c24e412011500c091ef484f73b1e69260bff3
Fabio Giolito <fabiogiolito@gmail.com>
2021-05-16 17:16:55 +0100

    expa layout

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,43 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
-export const config = {
-  unstable_runtimeJS: false
-};
+// Disable CSR
+export const config = { unstable_runtimeJS: false };
 
+// Pass host as prop
 export async function getServerSideProps(context) {
-  return {
-    props: { host: context.req.headers.host }
-  }
+  return { props: { host: context.req.headers.host } }
 }
 
 export default function Home({host}) {
   return (
-    <div className={styles.container}>
+    <main>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Coming soon from Expa • {host}</title>
+        <link href="/favicon.png" rel="shortcut icon" type="image/x-icon"/>
+        <link href="/expa-touch.png" rel="apple-touch-icon"/>
+        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <meta content={`Coming soon from Expa • ${host}`} property="og:title"/>
+        <meta content="/expa_og.png" property="og:image"/>
+        <meta content={`Coming soon from Expa • ${host}`} property="twitter:title"/>
+        <meta content="/expa_og.png" property="twitter:image"/>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to {host}!
-        </h1>
-      </main>
+      <a href={`https://expa.com/?ref=${host}`}>
+        <Image src="/expa.svg" width="100" height="21" />
+      </a>
 
-    </div>
+      <div className="content">
+        <h1>{host}</h1>
+        <p className="mono">Coming soon from <a href={`https://expa.com/?ref=${host}`}>Expa</a></p>
+      </div>
+
+      <div className="footer">
+        <hr />
+        <span><small>&copy;</small> Expa</span>
+        <a href={`mailto:hello@expa.com?subject=${host}&body=%0D%0A%0D%0A--%0D%0Acontact%20from%20${host}`}>Contact us</a>
+      </div>
+
+    </main>
   )
 }
