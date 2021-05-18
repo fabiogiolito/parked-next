698be81d0d27e604c311308c556e9a81641f9e89
Fabio Giolito <fabiogiolito@gmail.com>
2021-05-18 18:32:39 +0100

    add expa stuff back

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
-// Disable client side JS
+// Disable CSR
 export const config = { unstable_runtimeJS: false };
 
 // Pass host as prop
@@ -13,15 +14,33 @@ export default function Home({host}) {
     <main>
       <Head>
         <title>{host}</title>
-        <meta name="description" content="Coming soon" />
-        <meta content={host} property="og:title" />
-        <meta content={host} property="twitter:title" />
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Coming soon from Expa" />
+        <link href="/favicon.png" rel="shortcut icon" type="image/x-icon"/>
+        <link href="/expa-touch.png" rel="apple-touch-icon"/>
+        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <meta content={host} property="og:title"/>
+        <meta content="Coming soon from Expa" property="og:description"/>
+        <meta content="/expa_og.png" property="og:image"/>
+        <meta content={host} property="twitter:title"/>
+        <meta content="Coming soon from Expa" property="twitter:description"/>
+        <meta content="/expa_og.png" property="twitter:image"/>
       </Head>
+
+      <a href={`https://expa.com/?ref=${host}`}>
+        <img src="/expa.png" width="130" height="28" />
+      </a>
 
       <div className="content">
         <h1>{host}</h1>
-        <p className="mono">Coming soon</p>
+        <p className="mono">
+          Coming soon from <a href={`https://expa.com/?ref=${host}`}>Expa</a>
+        </p>
+      </div>
+
+      <div className="footer">
+        <hr />
+        <span><small>&copy;</small> Expa</span>
+        <a href={`mailto:hello@expa.com?subject=${host}&body=%0D%0A%0D%0A--%0D%0Acontact%20from%20${host}`}>Contact us</a>
       </div>
 
     </main>
